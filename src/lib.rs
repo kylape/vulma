367b03db36bd247424b44fe7906ea79897c66790
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-06-20 12:57:53 +0200

    Scan packages in a loop
    By default, packages will be scanned every hour. The interval can be
    adjusted with the -i,--interval argument or the VULMA_INTERVAL
    environment variable.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use core::time;
+use std::{process::Command, thread::sleep};
 
 use log::{debug, info};
 
@@ -8,7 +9,7 @@ use clap::Parser;
 use rpm::Rpm;
 
 #[derive(Debug, Parser)]
-pub struct Vulma {
+pub struct VulmaConfig {
     /// URL to forward the packages to
     #[arg(env = "VULMA_URL", default_value = "http://localhost:8080")]
     url: String,
@@ -20,39 +21,67 @@ pub struct Vulma {
     /// Path to the rpmdb
     #[arg(short, long, env = "VULMA_RPMDB", default_value = "/var/lib/rpm")]
     rpmdb: String,
+
+    /// Interval between package scanning in seconds
+    #[arg(short, long, env = "VULMA_INTERVAL", default_value_t = 3600)]
+    interval: u64,
 }
 
-pub fn run(args: Vulma) -> anyhow::Result<()> {
-    info!("Collecting package information...");
-    let mut rpm = Command::new("rpm");
-    rpm.args([
-        "--dbpath",
-        &args.rpmdb,
-        "-qa",
-        "--qf",
-        "%{NAME}|%{VERSION}|%{RELEASE}|%{ARCH}|%{SHA256HEADER}\n",
-    ]);
+struct Vulma {
+    url: Option<String>,
+    cmd: Command,
+}
 
-    let pkgs = rpm.output().context("Failed to run rpm command")?;
-    let stdout =
-        std::str::from_utf8(pkgs.stdout.as_slice()).context("Failed to parse rpm output")?;
+impl Vulma {
+    fn run(&mut self) -> anyhow::Result<()> {
+        info!("Collecting package information...");
+        let pkgs = self.cmd.output().context("Failed to run rpm command")?;
+        let stdout =
+            std::str::from_utf8(pkgs.stdout.as_slice()).context("Failed to parse rpm output")?;
 
-    info!("Parsing...");
-    let pkgs = stdout
-        .lines()
-        .map(str::parse::<Rpm>)
-        .collect::<Result<Vec<_>, _>>()
-        .context("Failed to parse package information")?;
-    debug!("{pkgs:?}");
+        info!("Parsing...");
+        let pkgs = stdout
+            .lines()
+            .map(str::parse::<Rpm>)
+            .collect::<Result<Vec<_>, _>>()
+            .context("Failed to parse package information")?;
+        debug!("{pkgs:?}");
 
-    info!("Sending updates...");
+        info!("Sending updates...");
 
-    if !args.skip_http {
-        reqwest::blocking::Client::new()
-            .post(args.url)
-            .json(&pkgs)
-            .send()
-            .context("Failed to post package information")?;
+        if let Some(url) = &self.url {
+            reqwest::blocking::Client::new()
+                .post(url)
+                .json(&pkgs)
+                .send()
+                .context("Failed to post package information")?;
+        }
+        Ok(())
     }
-    Ok(())
+}
+
+impl From<VulmaConfig> for Vulma {
+    fn from(cfg: VulmaConfig) -> Self {
+        let url = if !cfg.skip_http { Some(cfg.url) } else { None };
+        let mut cmd = Command::new("rpm");
+        cmd.args([
+            "--dbpath",
+            &cfg.rpmdb,
+            "-qa",
+            "--qf",
+            "%{NAME}|%{VERSION}|%{RELEASE}|%{ARCH}|%{SHA256HEADER}\n",
+        ]);
+
+        Vulma { url, cmd }
+    }
+}
+
+pub fn run(args: VulmaConfig) -> anyhow::Result<()> {
+    let interval = time::Duration::from_secs(args.interval);
+    let mut vulma: Vulma = args.into();
+
+    loop {
+        vulma.run()?;
+        sleep(interval);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use env_logger::Env;
-use vulma::Vulma;
+use vulma::VulmaConfig;
 
 fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(Env::default().filter_or("VULMA_LOGLEVEL", "info")).init();
-    let args = Vulma::parse();
+    let args = VulmaConfig::parse();
     vulma::run(args)
 }
